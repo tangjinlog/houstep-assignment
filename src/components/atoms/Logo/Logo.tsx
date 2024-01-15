cb987f39dfb9fff64fb36ebf1c03a629705e2484
tangjinlog <lkg70007@gmail.com>
2024-01-15 22:24:34 +0900

    feat: Logo atom 컴포넌트 추가 [#1]

---
+++ src/components/atoms/Logo/Logo.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import LogoBig from '@images/logo-black-lg.png';
+import LogoMid from '@images/logo-black-sm.png';
+
+interface LogoProps {
+	size: 'big' | 'mid';
+}
+
+function Logo({ size }: LogoProps) {
+	switch (size) {
+		case 'big':
+			return <Image src={LogoBig} width={150} height={51} alt="Home Logo" />;
+		case 'mid':
+			return <Image src={LogoMid} height={32} alt="Home Logo" />;
+	}
+}
+
+export default Logo;
