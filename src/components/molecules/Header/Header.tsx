0ad79263821faf025e8b48bdc74e118fd8a165cc
tangjinlog <lkg70007@gmail.com>
2024-01-16 14:36:17 +0900

    feat: Header molecules 컴포넌트 추가 [#2]

---
+++ src/components/molecules/Header/Header.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+import Logo from '@atoms/Logo';
+import { flexBetween } from '@styles/mixins';
+
+const Wrapper = styled.div`
+	${flexBetween};
+	align-items: center;
+	width: 100%;
+	height: 57px;
+	padding: var(--padding-xs);
+	background-color: var(--black);
+	box-shadow: var(--shadow-s);
+`;
+
+function Header() {
+	return (
+		<Wrapper>
+			<Logo size="mid" />
+		</Wrapper>
+	);
+}
+
+export default Header;
---
+++ src/pages/order.tsx
@@ -0,0 +1,11 @@
+import Header from '@molecules/Header';
+
+function order() {
+	return (
+		<>
+			<Header />
+		</>
+	);
+}
+
+export default order;
