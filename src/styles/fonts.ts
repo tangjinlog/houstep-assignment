4119f4298fa0253427bb26780f6318cd9dd7c163
tangjinlog <lkg70007@gmail.com>
2024-01-18 03:28:58 +0900

    chore: Roboto font 추가

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,13 +1,17 @@
 import { css } from '@emotion/react';
-import { Inter } from 'next/font/google';
+import { Inter, Roboto } from 'next/font/google';
 
 export const inter = Inter({
 	subsets: ['latin'],
 });
 
+export const roboto = Roboto({
+	subsets: ['latin'],
+	weight: ['400'],
+});
+
 /* Fonts */
 const common = css`
-	font-family: ${inter.style.fontFamily};
 	-webkit-font-smoothing: antialiased;
 `;
 
@@ -19,4 +23,12 @@ export const fontMid = css`
 export const fontBig = css`
 	${common};
 	font-size: 25px;
+`;
+
+export const orderTagText = css`
+	${common};
+	font-family: ${roboto.style.fontFamily};
+	font-size: 13px;
+	line-height: 18px;
+	letter-spacing: 0.16px;
 `;
