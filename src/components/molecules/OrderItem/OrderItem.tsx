72b1d723ae232181f4a07b49821e352f4f38a4ac
tangjinlog <lkg70007@gmail.com>
2024-01-18 03:35:41 +0900

    refactor: OrderItem 스타일링 & 합성 컴포넌트 구현 [#2]

--- src/components/molecules/OrderItem/OrderItem.tsx
+++ src/components/molecules/OrderItem/OrderItem.tsx
@@ -1,3 +1,41 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import Image from 'next/image';
+import { useState } from 'react';
+import { fontMid, orderTagText } from '@styles/fonts';
+import { flexBetween, flexCenter } from '@styles/mixins';
+import OrderCounter from '@molecules/OrderCounter';
+import OrderPlaceholder from '@images/order-placeholder.png';
+
+interface PropTypes extends React.HTMLAttributes<HTMLDivElement> {
+	isCounted: boolean;
+}
+
+const Wrapper = styled.div<PropTypes>`
+	display: flex;
+	gap: var(--gap-xs);
+	width: 100%;
+	padding: 9px 12px;
+	border: 1px solid rgba(0, 0, 0, 0.3);
+	border-radius: 10px;
+	${(props) =>
+		props.isCounted
+			? css`
+					background-color: #f75a2f1a;
+				`
+			: ''};
+`;
+
+const ContentWrapper = styled.div`
+	${flexBetween};
+	flex-direction: column;
+	width: 100%;
+`;
+
+const PriceWrapper = styled.div`
+	${flexBetween};
+`;
+
 interface OrderItemProps {
 	id: string;
 	name: string;
@@ -7,15 +45,60 @@ interface OrderItemProps {
 }
 
 function OrderItem({ id, name, event, materialType, price }: OrderItemProps) {
-	//TODO: Styling
+	const [isCounted, setIsCounted] = useState(false);
 	return (
-		<div>
-			<div>{name}</div>
-			<div>{event}</div>
-			<div>{materialType}</div>
-			<div>{price}</div>
-		</div>
+		<Wrapper isCounted={isCounted}>
+			<OrderItem.Image
+				src={OrderPlaceholder}
+				width={62}
+				height={62}
+				alt="OrderItem Image"
+			/>
+			<ContentWrapper>
+				<OrderItem.Title>
+					{name}
+					{!!event ? <OrderItem.EventTag>이벤트</OrderItem.EventTag> : null}
+				</OrderItem.Title>
+				<PriceWrapper>
+					<OrderCounter id={id} price={price} setIsCounted={setIsCounted} />
+					<OrderItem.Price>{price}원</OrderItem.Price>
+				</PriceWrapper>
+			</ContentWrapper>
+		</Wrapper>
 	);
 }
 
 export default OrderItem;
+
+OrderItem.Image = styled(Image)``;
+
+OrderItem.Title = styled.span`
+	${fontMid}
+	position: relative;
+	width: fit-content;
+	height: 23px;
+	padding-right: 9px;
+`;
+
+OrderItem.EventTag = styled.span`
+	${orderTagText};
+	// ${flexCenter};
+	display: inline-flex;
+	position: absolute;
+	left: 100%;
+	color: var(--white);
+	width: 53px;
+	height: 23px;
+	background-color: var(--tag-bg-1);
+	border-radius: 10px;
+`;
+
+OrderItem.Count = styled.span`
+	${fontMid}
+	display: inline-block;
+`;
+
+OrderItem.Price = styled.span`
+	${fontMid}
+	display: inline-block;
+`;
