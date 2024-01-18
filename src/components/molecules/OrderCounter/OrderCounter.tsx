80332cb28673c48be4f5b1791671bb192b1225cc
tangjinlog <lkg70007@gmail.com>
2024-01-19 03:33:21 +0900

    fix: order 합계 계산이 안되는 문제 수정 [#2]

--- src/components/molecules/OrderCounter/OrderCounter.tsx
+++ src/components/molecules/OrderCounter/OrderCounter.tsx
@@ -1,17 +1,11 @@
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
-import {
-	Dispatch,
-	SetStateAction,
-	useCallback,
-	useEffect,
-	useState,
-} from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import Button from '@atoms/Button';
 import { fontMid } from '@styles/fonts';
 import { orderListState } from '@states/atom';
-import { useRecoilState } from 'recoil';
-
+import { useSetRecoilState } from 'recoil';
+//TODO: useRouteControl 적용
 const buttonStyle = css`
 	${fontMid};
 	display: inline-block;
@@ -35,38 +29,70 @@ const DecrementButton = styled(Button)`
 	${buttonStyle}
 `;
 
+const CounterForm = styled.form``;
+
+const Input = styled.input`
+	width: 30px;
+	text-align: center;
+	&[type='number']::-webkit-inner-spin-button,
+	&[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+`;
+
 interface OrderCounterProps {
 	id: string;
 	price: number;
 	setIsCounted: Dispatch<SetStateAction<boolean>>;
 }
 
+type OrderTypes = { id: string; count: number; totalPrice: number };
+
 function OrderCounter({ id, price, setIsCounted }: OrderCounterProps) {
-	const [orderList, setOrderList] = useRecoilState(orderListState);
+	const setOrderList = useSetRecoilState(orderListState);
 	const [isClicked, setIsClicked] = useState(false);
 	const [count, setCount] = useState(0);
 
 	let orderMap = new Map();
 
-	useEffect(() => {
-		for (let item of orderList) {
-			orderMap.set(item.id, {
-				id: item.id,
-				count: item.count,
-				totalPrice: item.totalPrice,
+	const mapSetter = useCallback(
+		(map: Map<string, OrderTypes>, obj: OrderTypes, type?: 'inc' | 'dec') => {
+			if (type) {
+				return orderMap.set(id, {
+					id,
+					count: type === 'inc' ? count + 1 : count - 1,
+					totalPrice:
+						type === 'inc' ? price * (count + 1) : price * (count - 1),
+				});
+			}
+			return map.set(obj.id, {
+				id: obj.id,
+				count: obj.count,
+				totalPrice: obj.totalPrice,
 			});
-		}
-	}, [orderList]);
+		},
+		[count],
+	);
 
 	const updateOrder = useCallback(
 		(type: 'inc' | 'dec') => {
-			const updateMap = orderMap.set(id, {
-				id,
-				count: type === 'inc' ? count + 1 : count - 1,
-				totalPrice: type === 'inc' ? price * (count + 1) : price * (count - 1),
-			});
-			const updatedList = [...updateMap].flatMap(
-				([key, orderItem]) => orderItem,
+			const order = { id, count, totalPrice: price };
+			mapSetter(orderMap, order, type);
+			return orderMap.get(id);
+		},
+		[count],
+	);
+
+	const updateOrderList = useCallback(
+		(prevList: OrderTypes[], updatedOrder: OrderTypes) => {
+			for (let order of prevList) {
+				mapSetter(orderMap, order);
+			}
+			mapSetter(orderMap, updatedOrder);
+
+			const updatedList: OrderTypes[] = [...orderMap].flatMap(
+				([key, orderList]) => orderList,
 			);
 			return updatedList;
 		},
@@ -75,48 +101,57 @@ function OrderCounter({ id, price, setIsCounted }: OrderCounterProps) {
 
 	const incrementCount = useCallback(() => {
 		if (count < 999) {
+			setIsClicked(false);
 			setIsCounted(true);
 			setCount((prev) => prev + 1);
-			const items = updateOrder('inc');
-			setOrderList(items);
+			const updatedOrder = updateOrder('inc');
+			setOrderList((prevList) => updateOrderList(prevList, updatedOrder));
 		}
-	}, [count]);
+	}, [count, updateOrder]);
 
 	const decrementCount = useCallback(() => {
 		if (count > 0) {
+			if (count === 1) setIsCounted(false);
+			setIsClicked(false);
 			setCount((prev) => prev - 1);
-			const items = updateOrder('dec');
-			setOrderList(items);
-			if (count === 1) {
-				setIsCounted(false);
-			}
+			const updatedOrder = updateOrder('dec');
+			setOrderList((prevList) => updateOrderList(prevList, updatedOrder));
 		}
-	}, [count]);
+	}, [count, updateOrder]);
 
 	const handleSubmit = useCallback(
-		(e: any) => {
+		(e: React.FormEvent<HTMLFormElement>) => {
 			e.preventDefault();
 			setIsClicked(false);
+			const counts = Number(e.currentTarget.counter.value);
+			counts === 0 ? setIsCounted(false) : setIsCounted(true);
+
+			const updatedOrder = {
+				id,
+				count: count,
+				totalPrice: count * price,
+			};
+			setOrderList((prevList) => updateOrderList(prevList, updatedOrder));
 		},
 		[count],
 	);
-	//TODO: useInput 구현
+
 	return (
 		<Wrapper>
 			<DecrementButton onClick={decrementCount}>-</DecrementButton>
 			{!isClicked ? (
 				<Count onClick={() => setIsClicked((prev) => !prev)}>{count}</Count>
 			) : (
-				<form onSubmit={handleSubmit}>
-					<input
+				<CounterForm onSubmit={handleSubmit}>
+					<Input
 						type="number"
-						style={{ width: '30px', textAlign: 'center' }}
+						name="counter"
 						value={count}
 						onChange={(e) => setCount(Number(e.target.value))}
 						max={999}
 						autoFocus
 					/>
-				</form>
+				</CounterForm>
 			)}
 			<IncrementButton onClick={incrementCount}>+</IncrementButton>
 		</Wrapper>
